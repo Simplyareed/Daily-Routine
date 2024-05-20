a660b0d5a6fe61e57bdea3f2f8fe5d964ce2bf14
katzeundmais <zoellaphine@gmail.com>
2024-05-20 17:38:26 -0400

    added completed exercise button and logic to update graph

--- develop/public/js/graph.js
+++ develop/public/js/graph.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const Exercise = require('../models/Exercise');
-
 let d = new Date();
 let day = d.getDay();
-console.log(day);
+const completeBtn = document.querySelector('.completed-btn')
 
 document.addEventListener('DOMContentLoaded', function () {
     const chart = Highcharts.chart('chart', {
         chart: {
-            type: 'line'
+            type: 'bar'
         },
         title: {
             text: 'Weekly Productivity'
@@ -23,15 +20,63 @@ document.addEventListener('DOMContentLoaded', function () {
             categories: ['0', '1', '2', '3', '4']
         },
         series: [{
-            name: 'sample',
-            data: [0,2,4,6,1,9,9]
+            name: 'Sunday',
+            data: []
         }, {
-            name: 'data',
-            data: [6,3,1,6,4,3,2]
-        }]
+            name: 'Monday',
+            data: []
+        }, {
+            name: 'Tuesday',
+            data: []
+        }, {
+            name: 'Wednesday',
+            data: []
+        }, {
+            name: 'Thursday',
+            data: []
+        }, {
+            name: 'Friday',
+            data: []
+        }, {
+            name: 'Saturday',
+            data: []
+        }
+        ]
     })
 });
 
-async function generateData(userId, exerciseId, day) {
+// async function updateChart(chartId, userId, exerciseId, day) {
+//     const response = await fetch(`/localhost:3001?userId=${encodeURIComponent(userId)}&exerciseId=${encodeIRIComponent(exerciseId)}`);
+//     const data = await response.json();
+
+//     const chart = Highcharts.chart.find((chart) => chart.renderTo.id ===chartId);
     
-}
+//     if (chart) {
+//         chart.series[day].setData(data);
+//     }
+// }
+
+// document.getElementById('userId').addEventListener('change', function() {
+//     const exerciseId = document.getElementById('exerciseId').value;
+//     updateChart('chart', userId, this.value, exerciseId);
+// });
+
+// document.getElementById('exerciseId').addEventListener('change', function() {
+//     const userId = document.getElementById('userId').value;
+//     updateChart('chart', userId, this.value);
+// });
+
+//I believe this replaces above code
+completeBtn.addEventListener('click', async function() {
+    const userId = document.getElementById('userId').value;
+    const exerciseId = document.getElementById('exerciseId').value;
+
+    const response = await fetch(`/localhost:3001?userId=${encodeURIComponent(userId)}&exerciseId=${encodeIRIComponent(exerciseId)}`);
+    const data = await response.json();
+
+    const chart = Highcharts.chart.find((chart) => chart.renderTo.id === 'chart');
+
+    if (chart) {
+        chart.series[day].setData(data)
+    }
+})
