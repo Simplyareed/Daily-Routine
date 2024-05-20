5571be79030f506c1e28ae4d734cd3174d7b5e9f
Simplyareed <adam.reed1991@gmail.com>
2024-05-19 19:07:40 -0700

    signup code is completed, login code needs to be worked on

--- develop/server.js
+++ develop/server.js
@@ -39,11 +39,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
+// Use the API routes
+app.use('/api', apiRoutes);
+
+// app.get('/dashboard', (req, res) => {
 //   res.render('dashboard', { title: 'Dashboard' });
 // });
 
-app.get('/', (req, res) => {
+app.get('/dashboard', (req, res) => {
+  if (!req.session.loggedIn) {
+    return res.redirect('/login');
+  }
+  res.render('dashboard', { title: 'Dashboard' });
+});
+
+app.get('/login', (req, res) => {
   res.render('login', { title: 'Login Page' });
 });
 
